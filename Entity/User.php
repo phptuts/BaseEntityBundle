39eaae4eba97e302571bd21bcdccf4ed51bb952c
student <student@stedent-G750JX>
2015-06-09 20:36:38 -0500

    Made it an entity only bundle

---
+++ Entity/User.php
@@ -0,0 +1,143 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace NoahGlaser\EntityBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+
+/**
+ * Description of User
+ *
+ * @author student
+ */
+
+/**
+ * @ORM\MappedSuperclass()
+ * @ORM\HasLifecycleCallbacks()
+ */
+abstract class User extends Base implements AdvancedUserInterface
+{
+   /**
+    * @ORM\Column(name="email", type="string", length=255)
+    * @var string
+    */
+   private $email;
+   
+   /**
+    * @ORM\Column(name="password", type="string", length=255)
+    * @var string
+    */
+   private $password;
+      
+   /**
+    * @ORM\Column(name="isActive", type="boolean")
+    * @var string
+    */
+   private $isActive;
+   
+   /**
+    * @var array
+    *
+    * @ORM\Column(name="roles", type="array")
+    */
+   private $roles;
+   
+   /**
+    * @var string
+    * 
+    * @ORM\Column(name="secret_code", type="string", nullable=true)
+    */
+   private $secretCode;
+   
+   
+   public function getEmail() 
+   {
+      return $this->email;
+   }
+
+   public function getPassword()
+   {
+      return $this->password;
+   }
+
+   public function getIsActive() 
+   {
+      return $this->isActive;
+   }
+
+   public function getRoles() 
+   {
+      return $this->roles;
+   }
+
+   public function getSecretCode() 
+   {
+      return $this->secretCode;
+   }
+
+   public function setEmail($email) 
+   {
+      $this->email = $email;
+   }
+
+   public function setPassword($password) 
+   {
+      $this->password = $password;
+   }
+
+   public function setIsActive($isActive) 
+   {
+      $this->isActive = $isActive;
+   }
+
+   public function setRoles($roles) 
+   {
+      $this->roles = $roles;
+   }
+
+   public function setSecretCode($secretCode) 
+   {
+      $this->secretCode = $secretCode;
+   }
+
+   public function eraseCredentials()
+   {
+      ;
+   }
+
+   public function getSalt()
+   {
+      return null;
+   }
+
+   public function getUsername()
+   {
+      return $this->email;
+   }
+
+   public function isAccountNonExpired()
+   {
+      return true;
+   }
+
+   public function isAccountNonLocked() 
+   {
+      return true;
+   }
+
+   public function isCredentialsNonExpired() 
+   {
+      return true;
+   }
+
+   public function isEnabled() 
+   {
+      return $this->isActive;
+   }
+
+}
---
+++ spec/NoahGlaser/EntityBundle/Entity/UserSpec.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace spec\NoahGlaser\EntityBundle\Entity;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use NoahGlaser\EntityBundle\Entity\User;
+
+
+/**
+ * Description of UserSpec
+ *
+ * @author student
+ */
+class UserSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beAnInstanceOf('spec\NoahGlaser\EntityBundle\Entity\UserTest');
+    }
+    
+    function it_have_getters_setter_for_base_properties()
+    {
+        $this->setEmail('email');
+        $this->getEmail()->shouldBeLike('email');
+        
+        $this->setPassword('password');
+        $this->getPassword()->shouldBeLike('password');
+        
+        $this->setIsActive(true);
+        $this->getIsActive()->shouldBeLike(true);
+
+        $this->setRoles(['ROLE_USER']);
+        $this->getRoles()->shouldBeLike(['ROLE_USER']);
+
+        $this->setSecretCode('secret');
+        $this->getSecretCode()->shouldBeLike('secret');
+
+    }
+    
+    function it_should_set_return_is_active_when_is_enabled_is_called()
+    {
+        $this->setIsActive(true);
+        $this->isEnabled()->shouldBeLike(true);
+    }
+    
+    function it_get_salt_should_return_null()
+    {
+        $this->getSalt()->shouldBeLike(null);
+    }
+    
+    function it_get_username_should_return_email()
+    {
+        $this->setEmail('email');
+        $this->getUsername()->shouldBeLike('email');
+    }
+    
+    function it_should_erase_credentials_return_null()
+    {
+        $this->eraseCredentials()->shouldBeLike(null);
+    }
+    
+    function it_should_return_true_for_isAccountNonExpired_isAccountNonLocked_isCredentialsNonExpired()
+    {
+        $this->isAccountNonExpired()->shouldBeLike(true);
+        $this->isAccountNonLocked()->shouldBeLike(true);
+        $this->isCredentialsNonExpired()->shouldBeLike(true);
+    }
+}
+
+class UserTest extends User
+{
+    //put your code here
+}
